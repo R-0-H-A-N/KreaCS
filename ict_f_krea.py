5cad4b8c1745a6719dc7699593a7225ad961334f
Rohan <rohankalia05@gmail.com>
2023-10-27 02:49:20 +0530

    Comments #1

--- ict_f_krea.py
+++ ict_f_krea.py
@@ -3,15 +3,16 @@ import time
 import pickle
 import datetime
 import turtle
-
 # import pandas
-
 # import matplotlib
+
+# Most of the current imports are not doing much and are unnecessary but i do like to add them anyway as i see it as a good practice.
+# Do not worry about most of them as they are not really required unless you want to do very particular things.
 
 dict1 = {}
 
-
-def b1w():
+# Ignore these functions as they are not part of the course so far
+""" def b1w():
     o = open("file.dat", "wb")
     l = int(input("Enter the number of letters or numbers: "))
     l1 = []
@@ -31,9 +32,12 @@ def b1r():
 
 
 # b1w()
-# b1r()
+# b1r() """
+# You may now stop ignoring funcitons :).
 
-print(math.pi)
+
+# print(math.pi) <- If you would like to print pi. I use it to figure out if there is an error before this. If there is, this will not run and show me that the error is before this. 
+# If it does show, then it means the error is after this, making it easy to debug. 
 
 
 def addingBinaryNUmbers():
@@ -63,11 +67,10 @@ def loopsExamples():
 
 
 def turtlething():
-    mw = turtle.Screen()
-    mw.bgcolor("blue")
-    mp = turtle.Turtle()
-    mp.speed(30)
-    a = 0
+    mw = turtle.Screen() # Setting up screen to be able to change the backgroud color
+    mw.bgcolor("blue") # Changing the backgroud for aesthetic purposes
+    mp = turtle.Turtle() # Initialising the turtle module and setting it the variable mp, from this point forward mp will be used to perform actions on the turtle
+    mp.speed(30) # Setting the speed of the turtle
     n = 18
     for i in range(5):
         for i in range(n):
