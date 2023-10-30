7225a5fee8bd96f4566df5ef9396f5c787cdf2cf
Rohan <rohankalia05@gmail.com>
2023-10-30 13:02:51 +0530

    turtleintro added

--- ict_f_krea.py
+++ ict_f_krea.py
@@ -13,6 +13,8 @@ import turtle
 print(math.pi) # Printing pi. I use it to figure out if there is an error before this. If there is, this will not run.
 # If it does show, then it means the error is after this, making it easy to debug. 
 
+# I shall be using funcitons as blocks to talk about and showcase different things. If you have never done functions in programming before, don't worry. 
+# A function is a block of code which can be called in the code when we want it to run. For example, if i want to write the code for something but not run it immediately, i can put it in a function and run the funtion when i want to run it. 
 
 def addingBinaryNUmbers():
     print(
@@ -39,8 +41,20 @@ def loopsExamples():
 
 # loopsExamples()
 
+def turtleintro():
+    # Let us now talk about turtle
+    mp = turtle.Turtle() # Here mp is a variable that stores the turtle.Turtle() function which allows us to call from the turtle library.
+    mp.forward(100) # Tells the turtle to move forward by 100 pixels
+    mp.back(100) # Tells teh turtle to move back by 100 pixels
+    mp.left(90) # Tells the turtle to rotate left by 90 degrees
+    mp.right(90) # Tells the turtle to rotate right by 90 degrees
 
-def turtlething():
+    # Feel free to experiment with the code. 
+    # I would recommend getting comfortable with functions as well as it will help a lot in the future. 
+
+# turtleintro()
+
+def turtlesomewhatadvanced():
     mw = turtle.Screen() # Setting up screen to be able to change the backgroud color
     mw.bgcolor("blue") # Changing the backgroud for aesthetic purposes
     mp = turtle.Turtle() # Initialising the turtle module and setting it the variable mp, from this point forward mp will be used to perform actions on the turtle
@@ -101,4 +115,4 @@ def turtlething():
     turtle.done()
 
 
-# turtlething()
+# turtlesomewhatadvanced()
