d6f35d950ccced7bd27f2e12dff73a1d4f7740e8
Rohan <rohankalia05@gmail.com>
2023-12-19 19:53:32 +0530

    19/12/23 Class

--- ict_f_krea.py
+++ ict_f_krea.py
@@ -397,5 +397,17 @@ def sumlist_rec(a, i, s):
         s = s + a[i]
     return sumlist_rec(a, i+1, s+1)
 
-a = [1, 2, 3, 4, 5]
-print(sumlist_rec(a, 0, 0))
+""" a = [1, 2, 3, 4, 5]
+print(sumlist_rec(a, 0, 0)) """
+
+def TowerOfHanoi(n , s_pole, d_pole, i_pole):           
+    if n == 1:
+        print("Move disc 1 from pole",s_pole,"to pole",d_pole)
+        return
+    TowerOfHanoi(n-1, s_pole, i_pole, d_pole)
+    print("Move disc",n,"from pole",s_pole,"to pole",d_pole)
+    TowerOfHanoi(n-1, i_pole, d_pole, s_pole)
+ 
+""" n = 3
+TowerOfHanoi(n, 'A', 'C', 'B')
+ """
